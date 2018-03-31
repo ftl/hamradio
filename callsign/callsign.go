0776e632c7d60add5c06f757d1c0a21853639798
Florian Thienel <ftl@thecodingflow.com>
2018-03-31 19:34:08 +0200

    add a representation and some handling for callsigns

---
+++ callsign/callsign.go
@@ -0,0 +1,88 @@
+// Package callsign implements a representation and handling of callsigns.
+package callsign
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// Callsign represents a callsign consisting of a base call, a prefix, a suffix and the working condition:
+// Prefix/BaseCall/Suffix/WorkingCondition: K/DL1ABC/9/p
+// Prefix, suffix and working condition are optional and may be empty.
+type Callsign struct {
+	Prefix           string
+	BaseCall         string
+	Suffix           string
+	WorkingCondition string
+}
+
+var parseCallsignExpression = regexp.MustCompile("\\b(?:([A-Z0-9]+)/)?([A-Z0-9]?[A-Z][0-9][A-Z0-9]*[A-Z])(?:/([A-Z0-9]+))?(?:/(P|A|M|MM|AM))?\\b")
+var callsignWorkingConditions = map[string]bool{
+	"P":  true,
+	"A":  true,
+	"M":  true,
+	"MM": true,
+	"AM": true,
+}
+
+// Parse parses a callsign from a string.
+func Parse(s string) (*Callsign, error) {
+	normalString := strings.ToUpper(strings.TrimSpace(s))
+	if !parseCallsignExpression.MatchString(normalString) {
+		return &Callsign{}, fmt.Errorf("%q is not a valid callsign", s)
+	}
+
+	matches := parseCallsignExpression.FindAllStringSubmatch(normalString, 1)
+	if len(matches[0][0]) != len(normalString) {
+		return &Callsign{}, fmt.Errorf("%q is not a valid callsign", s)
+	}
+
+	callsign := Callsign{
+		Prefix:           matches[0][1],
+		BaseCall:         matches[0][2],
+		Suffix:           matches[0][3],
+		WorkingCondition: matches[0][4],
+	}
+
+	if _, ok := callsignWorkingConditions[callsign.Suffix]; ok && callsign.WorkingCondition == "" {
+		callsign.Suffix, callsign.WorkingCondition = callsign.WorkingCondition, callsign.Suffix
+	}
+
+	return &callsign, nil
+}
+
+func (callsign Callsign) String() string {
+	var buffer bytes.Buffer
+
+	if callsign.Prefix != "" {
+		buffer.WriteString(callsign.Prefix)
+		buffer.WriteString("/")
+	}
+	buffer.WriteString(callsign.BaseCall)
+	if callsign.Suffix != "" {
+		buffer.WriteString("/")
+		buffer.WriteString(callsign.Suffix)
+	}
+	if callsign.WorkingCondition != "" {
+		buffer.WriteString("/")
+		buffer.WriteString(strings.ToLower(callsign.WorkingCondition))
+	}
+	return buffer.String()
+}
+
+// FindAll returns all callsigns that are contained in the given string.
+func FindAll(s string) []*Callsign {
+	normalString := strings.ToUpper(strings.TrimSpace(s))
+	matches := parseCallsignExpression.FindAllStringSubmatch(normalString, -1)
+
+	callsigns := make([]*Callsign, 0, len(matches))
+	for _, match := range matches {
+		callsign, err := Parse(match[0])
+		if err == nil {
+			callsigns = append(callsigns, callsign)
+		}
+	}
+	return callsigns
+}
---
+++ callsign/callsign_test.go
@@ -0,0 +1,82 @@
+package callsign
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	testCases := []struct {
+		callsign string
+		expected Callsign
+		valid    bool
+	}{
+		{"K/DL1ABC/9/P", Callsign{Prefix: "K", BaseCall: "DL1ABC", Suffix: "9", WorkingCondition: "P"}, true},
+		{"DL1ABC/9/P", Callsign{Prefix: "", BaseCall: "DL1ABC", Suffix: "9", WorkingCondition: "P"}, true},
+		{"DL1ABC/9", Callsign{Prefix: "", BaseCall: "DL1ABC", Suffix: "9", WorkingCondition: ""}, true},
+		{"DL1ABC/P", Callsign{Prefix: "", BaseCall: "DL1ABC", Suffix: "", WorkingCondition: "P"}, true},
+		{"K/DL1ABC/P", Callsign{Prefix: "K", BaseCall: "DL1ABC", Suffix: "", WorkingCondition: "P"}, true},
+		{"K/DL1ABC", Callsign{Prefix: "K", BaseCall: "DL1ABC", Suffix: "", WorkingCondition: ""}, true},
+		{"K/DL1ABC/9", Callsign{Prefix: "K", BaseCall: "DL1ABC", Suffix: "9", WorkingCondition: ""}, true},
+		{"DL1ABC", Callsign{Prefix: "", BaseCall: "DL1ABC", Suffix: "", WorkingCondition: ""}, true},
+		{"", Callsign{}, false},
+		{"DLABC", Callsign{}, false},
+	}
+
+	for _, testCase := range testCases {
+		actual, err := Parse(testCase.callsign)
+		if testCase.valid {
+			if err != nil {
+				t.Errorf("parsing failed: %v", err)
+				continue
+			}
+			if *actual != testCase.expected {
+				t.Errorf("expected %v, but got %v", testCase.expected, *actual)
+			}
+		} else {
+			if err == nil {
+				t.Errorf("%v should raise an error", testCase.callsign)
+			}
+		}
+	}
+}
+func TestCallsignToString(t *testing.T) {
+	testCases := []struct {
+		callsign Callsign
+		expected string
+	}{
+		{Callsign{Prefix: "PRE", BaseCall: "BASE", Suffix: "SUF", WorkingCondition: "WC"}, "PRE/BASE/SUF/wc"},
+		{Callsign{Prefix: "", BaseCall: "BASE", Suffix: "SUF", WorkingCondition: "WC"}, "BASE/SUF/wc"},
+		{Callsign{Prefix: "", BaseCall: "BASE", Suffix: "SUF", WorkingCondition: ""}, "BASE/SUF"},
+		{Callsign{Prefix: "", BaseCall: "BASE", Suffix: "", WorkingCondition: "WC"}, "BASE/wc"},
+		{Callsign{Prefix: "PRE", BaseCall: "BASE", Suffix: "", WorkingCondition: "WC"}, "PRE/BASE/wc"},
+		{Callsign{Prefix: "PRE", BaseCall: "BASE", Suffix: "", WorkingCondition: ""}, "PRE/BASE"},
+		{Callsign{Prefix: "PRE", BaseCall: "BASE", Suffix: "SUF", WorkingCondition: ""}, "PRE/BASE/SUF"},
+		{Callsign{Prefix: "", BaseCall: "BASE", Suffix: "", WorkingCondition: ""}, "BASE"},
+	}
+
+	for _, testCase := range testCases {
+		s := testCase.callsign.String()
+		if s != testCase.expected {
+			t.Errorf("expected %q, but got %q", testCase.expected, s)
+		}
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	testCases := []struct {
+		s     string
+		count int
+	}{
+		{"", 0},
+		{"DL1ABC", 1},
+		{"DL1ABCDL1ABC", 1},
+		{"DL1ABC DL1ABC", 2},
+	}
+
+	for _, testCase := range testCases {
+		found := FindAll(testCase.s)
+		if len(found) != testCase.count {
+			t.Errorf("expected %d, but got %v", testCase.count, found)
+		}
+	}
+}
