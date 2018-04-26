a9cfe561306ad0c7d5c1c8896aa4a5d1c698ba3d
Florian Thienel <ftl@thecodingflow.com>
2018-04-26 18:01:22 +0200

    use github.com/ftl/localcopy

--- dxcc/file.go
+++ dxcc/file.go
@@ -1,118 +1,44 @@
 package dxcc
 
 import (
-	"bufio"
-	"fmt"
 	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-	"time"
+
+	"github.com/ftl/localcopy"
 )
-
-const httpTimeFormat = time.RFC1123
-const httpLastModified = "Last-Modified"
-
-var httpClient = &http.Client{
-	Timeout: time.Second * 10,
-}
 
 // LoadLocal loads a cty.dat file from the local file system.
 func LoadLocal(localFilename string) (*Prefixes, error) {
-	file, err := os.Open(localFilename)
+	prefixes, err := localcopy.LoadLocal(localFilename, func(r io.Reader) (interface{}, error) {
+		return Read(r)
+	})
 	if err != nil {
-		return &Prefixes{}, err
+		return nil, err
 	}
-	defer file.Close()
-
-	in := bufio.NewReader(file)
-	prefixes, err := Read(in)
-	if err != nil {
-		return &Prefixes{}, err
-	}
-	return prefixes, nil
+	return prefixes.(*Prefixes), nil
 }
 
 // LoadRemote loads a cty.dat file from a remote URL.
 func LoadRemote(remoteURL string) (*Prefixes, error) {
-	resp, err := httpClient.Get(remoteURL)
+	prefixes, err := localcopy.LoadRemote(remoteURL, func(r io.Reader) (interface{}, error) {
+		return Read(r)
+	})
 	if err != nil {
-		return &Prefixes{}, err
+		return nil, err
 	}
-	defer resp.Body.Close()
-
-	in := bufio.NewReader(resp.Body)
-	prefixes, err := Read(in)
-	if err != nil {
-		return &Prefixes{}, err
-	}
-	return prefixes, nil
+	return prefixes.(*Prefixes), nil
 }
 
 // Download downloads a cty.dat file from a remote URL and stores it locally.
 func Download(remoteURL, localFilename string) error {
-	response, err := httpClient.Get(remoteURL)
-	if err != nil {
-		return fmt.Errorf("failed to download cty.dat: %v", err)
-	}
-	defer response.Body.Close()
-
-	os.MkdirAll(filepath.Dir(localFilename), os.ModePerm)
-	localFile, err := os.Create(localFilename)
-	if err != nil {
-		return fmt.Errorf("failed to open local file: %v", err)
-	}
-	defer localFile.Close()
-
-	_, err = io.Copy(localFile, response.Body)
-	if err != nil {
-		return fmt.Errorf("failed to store cty.dat locally: %v", err)
-	}
-
-	return nil
-}
-
-// NeedsUpdate checks whether the local copy of a cty.dat file needs to be
-// updated from the given remote URL.
-func NeedsUpdate(remoteURL, localFilename string) (bool, error) {
-	response, err := httpClient.Head(remoteURL)
-	if err != nil {
-		return false, err
-	}
-	var lastModified time.Time
-	if lastModifiedHeader, ok := response.Header[httpLastModified]; ok {
-		if len(lastModifiedHeader) == 0 {
-			return false, fmt.Errorf("Last-Modified header is empty")
-		}
-
-		lastModified, err = time.Parse(httpTimeFormat, lastModifiedHeader[0])
-		if err != nil {
-			return false, fmt.Errorf("cannot parse Last-Modified header: %v", err)
-		}
-	} else {
-		return false, fmt.Errorf("response does not contain a Last-Modified header")
-	}
-
-	localFileInfo, err := os.Stat(localFilename)
-	if os.IsNotExist(err) {
-		return true, nil
-	} else if err != nil {
-		return false, err
-	}
-
-	return lastModified.After(localFileInfo.ModTime()), nil
+	return localcopy.Download(remoteURL, localFilename, func(r io.Reader) (interface{}, error) {
+		return Read(r)
+	})
 }
 
 // Update updates the local copy of a cty.dat file from the given remote URL,
 // but only if an update is needed.
 func Update(remoteURL, localFilename string) (bool, error) {
-	needsUpdate, err := NeedsUpdate(remoteURL, localFilename)
-	if err != nil {
-		return false, err
-	}
-
-	if !needsUpdate {
-		return false, nil
-	}
-	return true, Download(remoteURL, localFilename)
+	return localcopy.Update(remoteURL, localFilename, func(r io.Reader) (interface{}, error) {
+		return Read(r)
+	})
 }
--- dxcc/file_test.go
+++ dxcc/file_test.go
@@ -6,11 +6,16 @@ import (
 	"net/http/httptest"
 	"os"
 	"testing"
-	"time"
 )
 
+const testFilename = "./testdata/cty.dat"
+
+var serveCtyDat = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, testFilename)
+})
+
 func TestLoadLocal(t *testing.T) {
-	prefixes, err := LoadLocal("./testdata/cty.dat")
+	prefixes, err := LoadLocal(testFilename)
 	if err != nil {
 		t.Errorf("loading failed: %v", err)
 		t.FailNow()
@@ -58,56 +63,3 @@ func TestDownload(t *testing.T) {
 		t.Errorf("expected file size %d, but got %d", testFileInfo.Size(), tempFileInfo.Size())
 	}
 }
-
-func TestNeedsUpdate(t *testing.T) {
-	tempFile, err := ioutil.TempFile("", "dxcc.TestDownload")
-	if err != nil {
-		t.Errorf("failed to create temp file: %v", err)
-		t.FailNow()
-	}
-	defer tempFile.Close()
-	defer os.Remove(tempFile.Name())
-	tempFileInfo, _ := os.Stat(tempFile.Name())
-
-	timeToServe := time.Now()
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !timeToServe.IsZero() {
-			w.Header().Add(httpLastModified, timeToServe.Format(httpTimeFormat))
-		}
-	}))
-	defer testServer.Close()
-
-	testCases := []struct {
-		remoteFileTime time.Time
-		needsUpdate    bool
-	}{
-		{time.Time{}, false},
-		{tempFileInfo.ModTime().Add(-10 * time.Minute), false},
-		{tempFileInfo.ModTime().Add(10 * time.Minute), true},
-	}
-
-	for _, testCase := range testCases {
-		timeToServe = testCase.remoteFileTime
-
-		needsUpdate, err := NeedsUpdate(testServer.URL, tempFile.Name())
-		if timeToServe.IsZero() {
-			if err == nil {
-				t.Errorf("missing Last-Update header should raise an error")
-			}
-		} else {
-			if err != nil {
-				t.Errorf("failed to check for update: %v", err)
-				t.FailNow()
-			}
-			if needsUpdate != testCase.needsUpdate {
-				t.Errorf("expected needsUpdate %t, but got %t for %v", testCase.needsUpdate, needsUpdate, timeToServe.Sub(tempFileInfo.ModTime()))
-			}
-		}
-	}
-}
-
-const testFilename = "./testdata/cty.dat"
-
-var serveCtyDat = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, testFilename)
-})
